bf8f2a14f54eed95d2b48c4a8e57e56e8fca9981
GwonHeeJun <gwonhj9751@naver.com>
2020-03-23 17:42:38 +0900

    [UPDATE] 작업 예정인 라우터 정의
    - 담당자가 개발을 좀더 쉽게 하기 위해 개발 진행에 필요한 라우터 정의
    - 이외의 라우터는 개발리더에게 상의 후 추가할것,

--- src/Router/Router.tsx
+++ src/Router/Router.tsx
@@ -5,8 +5,12 @@ import { Route, Switch } from 'react-router-dom';
 const Router: React.FC = () => {
   return (
     <Switch>
-      <Route exact path="/" component={() => <div>main</div>} />
-      <Route exact path="/home" component={() => <div>home</div>} />
+      <Route exact path="/" component={() => <div>로그인 페이지</div>} />
+      <Route exact path="/regist" component={() => <div>회원가입 페이지</div>} />
+      <Route exact path="/home" component={() => <div>메인 페이지</div>} />
+      <Route exact path="/laptop" component={() => <div>노트북 대여 페이지</div>} />
+      <Route exact path="/notice" component={() => <div>공지사항 페이지</div>} />
+      <Route exact path="/notice/write" component={() => <div>공지사항 작성 페이지</div>} />
     </Switch>
   );
 };
